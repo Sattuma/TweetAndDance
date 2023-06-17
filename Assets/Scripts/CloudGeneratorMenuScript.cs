c90b8a1960470038493d91e135b8935f87418d6f
Sattuma <92377727+Sattuma@users.noreply.github.com>
2023-06-18 00:43:46 +0300

    18.6.2023
    Kaikkee pientä säätämistä

---
+++ Assets/Scripts/CloudGeneratorMenuScript.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGeneratorMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] clouds;
+
+    [SerializeField]
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject endPoint;
+
+    Vector3 startPos;
+
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startPos = transform.position;
+        Prewarm();
+        Invoke("AttemptSpawn", spawnInterval);
+
+    }
+
+    void SpawnCloud(Vector3 startPos)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, 4);
+        GameObject cloud = Instantiate(clouds[randomIndex]);
+
+        float startY = UnityEngine.Random.Range(startPos.y - minY, startPos.y + maxY);
+        cloud.transform.position = new Vector3(startPos.x, startY, startPos.z);
+
+        float scale = UnityEngine.Random.Range(0.2f, 1f);
+        cloud.transform.localScale = new Vector2(scale, scale);
+
+        float speed = UnityEngine.Random.Range(minSpeed, maxSpeed);
+        cloud.GetComponent<CloudScript>().StartFloating(speed, endPoint.transform.position.x);
+
+
+    }
+
+    void AttemptSpawn()
+    {
+        //check some things.
+        SpawnCloud(startPos);
+
+        Invoke("AttemptSpawn", spawnInterval);
+
+    }
+
+    void Prewarm()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Vector3 spawnPos = startPos + Vector3.right * (i * 2);
+            SpawnCloud(spawnPos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
