890f7dc261fa50d0cadd6f1e826ba58155779b71
Sattuma <92377727+Sattuma@users.noreply.github.com>
2023-06-23 11:35:54 +0300

    23.6.2023
    Menu kentän partikkeliefut lisätty ja painikkeiden liikkeet rauhotettu.
    Game leveliin tekstit korjattu. Ja ehkä jotain muuta pientä mitä en nyt muista. :D

--- Assets/Scripts/SailboatScript.cs
+++ Assets/Scripts/SailboatScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SailboatScript : MonoBehaviour
 {
 
-    public float speed = 3f;
+    public float speed = 3;
 
     // Start is called before the first frame update
     void Start()
