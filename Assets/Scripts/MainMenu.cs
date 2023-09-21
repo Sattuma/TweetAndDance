bffb8b09259ae9bd795d8a326ac01d0e98d66f7f
SirHenry001 <henri.j.heikkila@gmail.com>
2023-09-21 16:35:05 +0300

    code commit 21.9
    main menu basis update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("MainMenu")]
+
+    [Header("MainMenu Buttons")]
     public GameObject startButton;
     public GameObject settingButton;
     public GameObject howToPlayButton;
     public GameObject creditsButton;
     public GameObject backButton;
     public GameObject quitButton;
-
     public GameObject mainButtons;
 
-
-    [Header("Display Windows")]
+    [Header("Settings Window")]
     public GameObject settingWindow;
+
+    [Header("HowToPlay Window")]
+    public GameObject howToPlayWindow;
+    public GameObject[] howToPlayImagesKey;
+    public GameObject[] howToPlayImagesController;
+    public GameObject howToPlayNavigationButtonLeft, howToPlayNavigationButtonRight;
+
+    [Header("Credits Window")]
+    public GameObject creditsWindow;
+
+
     public GameObject logo;
 
-
-
-    private void Start()
+    public void OnPointerEnter()
     {
-        Time.timeScale = 1;
-        AudioManager.instance.PlayMusicFX(0);
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 
     public void ContinueGame()
@@ -46,20 +57,77 @@ public class MainMenu : MonoBehaviour
     public void OpenSettings()
     {
         Debug.Log("Settings opened");
+        
         mainButtons.SetActive(false);
         backButton.SetActive(true);
         settingWindow.SetActive(true);
         logo.SetActive(false);
+        //GameObject.Find("Master").GetComponent<Selectable>().Select();
     }
 
     public void OpenHowToPlay()
     {
         Debug.Log("HowToPlay Opened");
+        
+        mainButtons.SetActive(false);
+        backButton.SetActive(true);
+        howToPlayWindow.SetActive(true);
+        logo.SetActive(false);
+        howToPlayNavigationButtonRight.SetActive(true);
+        howToPlayNavigationButtonLeft.SetActive(false);
+
+        for(int i = 0; i < howToPlayImagesController.Length; i++)
+        {
+            howToPlayImagesController[i].SetActive(false);
+        }
+        for (int i = 0; i < howToPlayImagesKey.Length; i++)
+        {
+            howToPlayImagesKey[i].SetActive(true);
+        }
+
+
+
+
     }
 
+    public void HowToPlayRightNav()
+    {
+        howToPlayNavigationButtonRight.SetActive(false);
+        howToPlayNavigationButtonLeft.SetActive(true);
+
+        for (int i = 0; i < howToPlayImagesController.Length; i++)
+        {
+            howToPlayImagesController[i].SetActive(true);
+        }
+        for (int i = 0; i < howToPlayImagesKey.Length; i++)
+        {
+            howToPlayImagesKey[i].SetActive(false);
+        }
+    }
+
+    public void HowToPlayLeftNav()
+    {
+        howToPlayNavigationButtonRight.SetActive(true);
+        howToPlayNavigationButtonLeft.SetActive(false);
+
+        for (int i = 0; i < howToPlayImagesController.Length; i++)
+        {
+            howToPlayImagesController[i].SetActive(false);
+        }
+        for (int i = 0; i < howToPlayImagesKey.Length; i++)
+        {
+            howToPlayImagesKey[i].SetActive(true);
+        }
+    }
     public void OpenCredits()
     {
         Debug.Log("Credits Opened");
+
+        mainButtons.SetActive(false);
+        backButton.SetActive(true);
+        creditsWindow.SetActive(true);
+        logo.SetActive(false);
+
     }
 
     public void OpenBack()
@@ -68,7 +136,10 @@ public class MainMenu : MonoBehaviour
         mainButtons.SetActive(true);
         backButton.SetActive(false);
         settingWindow.SetActive(false);
+        howToPlayWindow.SetActive(false);
+        creditsWindow.SetActive(false);
         logo.SetActive(true);
+        startButton.GetComponent<Selectable>().Select();
     }
 
     public void QuitGame()
